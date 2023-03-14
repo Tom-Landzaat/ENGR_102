2ca8a44009477d4b77a28b2b55c6ff0b66e84b35
Tom Landzaat <tomlandzaat562@gmail.com>
2023-03-13 21:17:44 -0700

    updates 3/13/23

--- Dij for project.cpp
+++ Dij for project.cpp
@@ -11,10 +11,14 @@ int V , src , cost[100][100] ;
 int dist[100] ;
 bool visited[100] = {0} ;
 //keeps track of visited with T/F, all default as unvisted
+int parent[100] ;
+
 
 void basic(){
-    for(int i = 0;i <V ; i++)
+    for(int i = 0;i <V ; i++){
     dist[i] = INF ;
+    parent[i] = i;
+    }
     //distance from src to itself is 0
     dist[src] = 0 ;
 }
@@ -35,18 +39,49 @@ int getnearest() {
 
 void dijalgo(){
     //run for V-1 number of times to hit every vertex (starting at src)
-    for(int i =0 ; i < V-1 ; i++){
+    for(int i = 0 ; i < V-1 ; i++){
         int nearest = getnearest() ;
-        visited[nearest] ;
+        visited[nearest] = true ;
+
+        for(int adjacent = 0 ; adjacent < V ; adjacent++){
+            if(cost[nearest][adjacent] != INF and dist[adjacent]>dist[nearest]+cost[nearest][adjacent]){
+                dist[adjacent] = dist[nearest]+cost[nearest][adjacent] ;
+                //if the an adjacent vertex is not infinity and it is less than the other options then you update the adjacent node as the new one
+                parent[adjacent] = nearest ;
+                //update the parent vertex when we move to the next one
+            }
+        }
     }
 }
 
-int main(){
+void display(){
+    cout<<"Node:\t\t\tCost  :\t\t\tPath"    ;
+
+    for(int i = 0; i < V; i++){
+        cout<<i<<"\t\t\t"<<dist[i]<<"\t\t\t"<<" " ;
+
+        cout << i <<" ";
+        int parnode = parent[i];
+        
+        while(parnode != src){
+            cout<<" <- " << parnode << " ";
+            parnode = parent[parnode];
+        }
+        cout<<endl ;
+    }
+}
+
+int main(void){
+    cout << "Enter the number of vertices : " ;
     cin >> V ;
     for(int i = 0 ; i < V ; i++){
         for(int j = 0 ; j < V ; j++){
             cin >> cost[i][j] ;
         }
     }
+    cout<<"source node : " ;
     cin >> src ;
+    basic() ;
+    dijalgo() ;
+    display();
 }
